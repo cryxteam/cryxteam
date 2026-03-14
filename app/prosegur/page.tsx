5fff72260c65af8fda7012fd646a783bf5d5c3f6
cryxteam <proyectostteam@gmail.com>
2026-03-14 07:10:14 -0500

    Seguidoress

--- app/prosegur/page.tsx
+++ app/prosegur/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image'
 import Link from 'next/link'
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 import { usePathname } from 'next/navigation'
 import { supabase } from '@/lib/supabaseClient'
 import styles from './prosegur.module.css'
